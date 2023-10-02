4b66f7b64bc5accd5b2aa37a4a737f3f5fd3ecc6
leandro.nares@gmail.com <leandro.nares@gmail.com>
2023-10-02 20:34:05 -0300

    137. Map #02_V2

--- array/map2.js
+++ array/map2.js
@@ -7,7 +7,7 @@ const carrinho = [
 
 //Retornar um array apenas com os preços
 const paraObjeto = json => JSON.parse(json)
-const apenasPreco = produto => produto.preco
+const apenasPreco = produto => produto.preço
 
 const resultado = carrinho.map(paraObjeto).map(apenasPreco)
 console.log(resultado)
