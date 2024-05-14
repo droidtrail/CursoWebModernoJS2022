cf668466a005f8febdc810bd1fdf51bfaf57f7c8
Leandro Pereira <leandro.nares@gmail.com>
2024-05-14 14:17:26 -0300

    225. Formulário #01

---
+++ ExerciciosWeb/html/exercicios/form/server.js
@@ -0,0 +1,11 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.post('/usuarios',(req, resp)=>{
+    console.log(req.body)
+    resp.send('<h1>Parabéns!</h1>')
+})
+app.listen(3003)
